a4cb619a85d1c297ab686bc49a73ea62f5950a7b
chauhan-abhi <chauhanabhi1245@gmail.com>
2018-10-09 00:25:40 +0530

    generate message utility

--- server/server.js
+++ server/server.js
@@ -3,7 +3,14 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socketIO = require('socket.io')
+const {generateMessage} = require('./utils/message')
 
+
+//path provided to express  static middleware
+// old way
+//console.log(__dirname + '/../public')
+//better way 
+//console.log(publicPath)
 //prevent from first going into server directory then going out and then to public
 const publicPath = path.join(__dirname, '../public')
 const port = process.env.PORT || 8000
@@ -18,20 +25,9 @@ var io = socketIO(server)
 io.on('connection', (socket) => {
      console.log('New user connected')
 
-     socket.emit('newMessage', {
-        from: 'Admin',
-        text: 'Welcome to chat app',
-        createdAt: new Date().getTime() 
+     socket.emit('newMessage', generateMessage('Admin', 'Welcome to the CHAT App'))
 
-    })
-
-    socket.broadcast.emit('newMessage', {
-        from: 'Admin',
-        text: "New user joined",
-        createdAt: new Date().getTime() 
-
-    })
-
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'))
 
     //emit function can be used on both client and server
     /******* emit message to single connection ***************/
@@ -41,18 +37,12 @@ io.on('connection', (socket) => {
     //     completedAt: 1234
     // })
 
-
-
     // data in event from client to server
     // listen to create Message
     socket.on('createMessage', (message) => {
         console.log('createMessage', message)
         /********emit event to every single connection **********/
-        io.emit('newMessage', {
-            from: message.from,
-            text: message.text,
-            createdAt: new Date().getTime()    
-        })
+        io.emit('newMessage', generateMessage(message.from, message.text))
         /*******send event to everybody but this socket *************/
         // socket.broadcast.emit('newMessage', {
         //     from: message.from,
@@ -66,22 +56,13 @@ io.on('connection', (socket) => {
     })
 })
 
-
-
-//path provided to express  static middleware
-// old way
-//console.log(__dirname + '/../public')
-//better way 
-//console.log(publicPath)
-
 //configure middleware 
 app.use(express.static(publicPath))
 
-/*****we will use http server instead of express server******/
 // app.listen(port, () => {
 //     console.log(`Server is up on ${port}`)
 // })
-
+/*****we will use http server instead of express server******/
 server.listen(port, () => {
     console.log(`Server is up on ${port}`)
 })
